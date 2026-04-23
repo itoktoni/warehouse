4efc7214368e5dcb19c136d19522a34abc4a7372
itok toni <itok.toni@gmail.com>
2026-04-23 21:38:44 +0700

    update

--- resources/views/pages/barang/form.blade.php
+++ resources/views/pages/barang/form.blade.php
@@ -6,7 +6,11 @@
             <div class="row">
                 @bind($model)
 
-                <x-form-input col="3" name="barang_code" label="Kode Barang"/>
+                <div class=" form-group col-md-3 ">
+                    <label for="auto_id_barang_code">Kode Barang</label>
+                    <input class="form-control col-md-3" {{ request()->segment(5) === 'update' ? 'readonly' : '' }} type="text" value="{{ $model->barang_code ?? null }}" name="barang_code">
+                </div>
+
                 <x-form-select col="3" name="barang_id_category" label="Kategori" :options="$category"/>
                 <x-form-input col="4" name="barang_nama" />
 
